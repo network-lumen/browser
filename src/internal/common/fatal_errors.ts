25b69eed864e39391078c41b0946fb534437e3af
Ben <ben@lumen.invalid>
2026-06-17 08:08:11 +0200

    Continue cleaning (fatal errors)

--- src/internal/common/fatal_errors.ts
+++ src/internal/common/fatal_errors.ts
@@ -4,30 +4,31 @@ const lumen_api: any = (window as any).lumen;
 const gateway_lumen_api = lumen_api?.gateway;
 const profiles_lumen_api = lumen_api?.profiles;
 
-if(!lumen_api) {
-  throw new Error("FATAL000001");
-}
-if(!gateway_lumen_api) {
-  throw new Error("FATAL000002");
-}
-if(!profiles_lumen_api) {
-  throw new Error("FATAL000003");
-}
-
 async function checkLumenAPIReferences(){
     if(!lumen_api) throw new Error("FATAL000001");
     if(!gateway_lumen_api) throw new Error("FATAL000002");
     if(!profiles_lumen_api) throw new Error("FATAL000003");
 
+    /** lumen_api..... */
     if(!lumen_api.driveOnHlsProgress || typeof lumen_api.driveOnHlsProgress != "function" ) throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveOnHlsProgress'))
     if(!lumen_api.driveOnHlsArchiveProgress || typeof lumen_api.driveOnHlsArchiveProgress != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveOnHlsArchiveProgress'))
     if(!lumen_api.ipfsOnPublicGatewayPropagationProgress || typeof lumen_api.ipfsOnPublicGatewayPropagationProgress != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('ipfsOnPublicGatewayPropagationProgress'))
-    if(!lumen_api.driveBackup.encryptSnapshot || typeof lumen_api.driveBackup.encryptSnapshot != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveBackup.encryptSnapshot'))
-    if(!lumen_api.driveBackup.decryptSnapshot || typeof lumen_api.driveBackup.decryptSnapshot != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveBackup.decryptSnapshot'))
     if(!lumen_api.driveCancelHlsConvert || typeof lumen_api.driveCancelHlsConvert != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveCancelHlsConvert'))
     if(!lumen_api.driveCancelHlsArchiveDownload || typeof lumen_api.driveCancelHlsArchiveDownload != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveCancelHlsArchiveDownload'))
     if(!lumen_api.driveDownloadHlsArchive || typeof lumen_api.driveDownloadHlsArchive != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveDownloadHlsArchive'))
     if(!lumen_api.httpHead || typeof lumen_api.httpHead != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('httpHead'))
+
+    /** lumen_api.driveBackup..... */
+    if(!lumen_api.driveBackup.encryptSnapshot || typeof lumen_api.driveBackup.encryptSnapshot != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveBackup.encryptSnapshot'))
+    if(!lumen_api.driveBackup.decryptSnapshot || typeof lumen_api.driveBackup.decryptSnapshot != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('driveBackup.decryptSnapshot'))
+
+    /** lumen_api.gateway..... */
+    if(!gateway_lumen_api.onIngestProgress || typeof gateway_lumen_api.onIngestProgress != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('gateway.onIngestProgress'))
+    if(!gateway_lumen_api.renameCid || typeof gateway_lumen_api.renameCid != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('gateway.renameCid'))
+
+    /** lumen_api.profiles... */ 
+    if(!profiles_lumen_api.getActive || typeof profiles_lumen_api.getActive != "function") throw new Error (FATAL_ERROR_MAP['FATAL000004']('profiles.getActive'))
+
 }
 
 const FATAL_ERROR_MAP: Record<string, any> = {
@@ -41,8 +42,3 @@ export {
     checkLumenAPIReferences,
     FATAL_ERROR_MAP
 }
-
-/**
- * gateway_lumen_api?.onIngestProgress
- * gateway_lumen_api.renameCid
- */
